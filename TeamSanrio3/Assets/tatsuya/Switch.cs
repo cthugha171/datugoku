c672f8db0036a6949b1645e954bdd92efc0b50eb
Cthugha <tatsusato0519@gmail.com>
2021-05-24 16:04:18 +0900

    switchのアニメーションと当たり判定修正

--- TeamSanrio3/Assets/tatsuya/Switch.cs
+++ TeamSanrio3/Assets/tatsuya/Switch.cs
@@ -7,9 +7,19 @@ public class Switch: MonoBehaviour
     [SerializeField] private string plTag = "Player";
     [SerializeField] private GameObject Laser;
     [SerializeField] private Animator animator;
+    [SerializeField] private BoxCollider collider;
+    [SerializeField] private Vector3 colSizeOn;
+    [SerializeField] private Vector3 colSizeOff;
+    [SerializeField] private Vector3 colCenterOn;
+    [SerializeField] private Vector3 colCenterOff;
 
     //設定したゲームオブジェクトのアクティブ設定と同じにする
     [SerializeField] private bool preSet = true;
+
+    void Start()
+    {
+        animator.SetBool("switching", preSet);
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -18,6 +28,16 @@ public class Switch: MonoBehaviour
             preSet = !preSet;
             animator.SetBool("switching", preSet);
             Laser.SetActive(preSet);
+        }
+        if (preSet==false)
+        {
+            collider.center = colCenterOn;
+            collider.size = colSizeOn;
+        }
+        else
+        {
+            collider.center = colCenterOff;
+            collider.size = colSizeOff;
         }
     }
 }
